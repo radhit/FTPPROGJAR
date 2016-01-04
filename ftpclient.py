c8cebfcf4e089a3a74f739f5033dead5485de32b
LusianaNurul <lusiananurulaini@gmail.com>
2016-01-04 16:45:04 +0700

    atur autentifikasi, help dan ubah nama

---
+++ ftpclient.py
@@ -0,0 +1,44 @@
+import sys
+import os,socket,threading,time
+
+server_address = ('127.0.0.1', 8000)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(server_address)
+
+data = ""
+data = client.recv(1024)
+print data
+user = ""
+password = ""
+
+while(1):
+	cmd = raw_input(">>")
+
+	if "USER" in cmd:
+		user = cmd.split(" ")[1]
+		cmd = client.send(cmd+"\r\n")
+		data = client.recv(1024)
+		print data
+	elif "PASS" in cmd:
+		password = cmd.split(" ")[1]
+		cmd = client.send(cmd+"\r\n")
+		data = client.recv(1024)
+		print data
+	elif "RNFR" in cmd:
+		cmd = client.send(cmd+"\r\n")
+		data = client.recv(1024)
+		print data
+	elif "RNTO" in cmd:
+		cmd = client.send(cmd+"\r\n")
+		data = client.recv(1024)
+		print data
+	elif "HELP" in cmd:
+		cmd = client.send(cmd+"\r\n")
+		data=client.recv(1024)
+		print data	
+
+
+
+
+
+
---
+++ ftpserver.py
@@ -0,0 +1,108 @@
+import os,socket,threading,time
+import subprocess
+
+allow_delete = True
+#local_ip = socket.gethostbyname(socket.gethostname())
+local_ip = "127.0.0.1"
+local_port = 8000
+currdir=os.path.abspath('.')
+
+
+class FTPserverThread(threading.Thread):
+    def __init__(self,(conn,addr)):
+        self.conn=conn
+        self.addr=addr
+        self.basewd=currdir
+        self.cwd=self.basewd
+        self.rest=True
+        self.pasv_mode=True
+        threading.Thread.__init__(self)
+        self.flagu = 0
+        self.flagp = 0
+
+    def run(self):
+        self.conn.send('220 Welcome!\r\n')
+        while True:
+            cmd=self.conn.recv(512)
+            if not cmd: break
+            else:
+                print 'Recieved:',cmd
+                try:
+                    func=getattr(self,cmd[:4].strip().upper())
+                    func(cmd)
+                except Exception,e:
+                    print 'ERROR:',e
+                    self.conn.send('500 Sorry.\r\n')
+
+    def USER(self,cmd):
+        user=cmd.strip().split()[1]
+        if user == "joke":
+            self.flagu = 1
+            self.conn.send('331 OK.\r\n')
+        else:
+            self.conn.send('530 sorry.\r\n Masukan Username yang benar.\r\n')
+            #raise SystemExit
+
+    def PASS(self,cmd):
+        password=cmd.strip().split()[1]
+        if password == "fun":
+            self.flagp = 1
+            self.conn.send('331 OK.\r\n')
+        else:
+            self.conn.send('530 sorry.\r\n Masukan Password yang benar.\r\n')
+            #raise SystemExit
+
+    def RNFR(self,cmd):
+        if self.flagu==1 and self.flagp==1:
+            self.rnfn=os.path.join(self.cwd,cmd[5:-2])
+            self.conn.send('350 Ready.\r\n')
+        else:
+            self.conn.send('Masukan Username dan Password dahulu')
+
+    def RNTO(self,cmd):
+        if self.flagu==1 and self.flagp==1:
+            fn=os.path.join(self.cwd,cmd[5:-2])
+            os.rename(self.rnfn,fn)
+            self.conn.send('250 File renamed.\r\n')
+        else:
+            self.conn.send('Masukan Username dan Password dahulu')
+
+    def HELP(self,cmd):
+        cmdlist = ( "CWD    --  Mengubah direktori aktif\n"
+                    "QUIT   --  Keluar aplikasi\n"
+                    "RETR   --  Mengunduh file\n"
+                    "STOR   --  Mengunggah file\n"
+                    "RNFR   --  Mengganti nama file\n"
+                    "RNTO   --  Mengganti nama file\n"
+                    "DELE   --  Menghapus file\n"
+                    "RMD    --  Menghapus direktori\n"
+                    "MKD    --  Membuat direktori\n"
+                    "PWD    --  Mencetak direktori aktif\n"
+                    "LIST   --  Mendaftar file dan direktori\n"
+                    "HELP   --  Menampilkan daftar perintah\n")
+        self.conn.send(cmdlist)
+
+
+class FTPserver(threading.Thread):
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((local_ip,local_port))
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.sock.listen(5)
+        while True:
+            th=FTPserverThread(self.sock.accept())
+            th.daemon=True
+            th.start()
+
+    def stop(self):
+        self.sock.close()
+
+if __name__=='__main__':
+    ftp=FTPserver()
+    ftp.daemon=True
+    ftp.start()
+    print 'On', local_ip, ':', local_port
+    raw_input('Enter to end...\n')
+    ftp.stop()
